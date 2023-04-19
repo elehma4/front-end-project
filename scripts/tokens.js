3953788fab29f8c2f9d012bb639ff6c84b56b86b
Hunter Atkins <lsatkins111@gmail.com>
2023-04-19 16:05:21 -0500

    added in all api calls for tokens page

--- scripts/tokens.js
+++ scripts/tokens.js
@@ -1,12 +1,65 @@
 // API link: https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cusd-coin%2Cdogecoin%2Carbitrum%2Cgmx%2Cmagic&vs_currencies=usd
 
+
+//global variables
 let numbers = document.querySelector(".numbers");
 let coinNames = document.querySelector(".coinNames");
 let prices = document.querySelector(".prices");
+let marketCap = document.querySelector(".marketCap");
+let volumes = document.querySelector(".volume");
+let changes = document.querySelector(".change");
+
+//functions
+
+// function to convert to shortened number with letter suffix
+let shortNum = (num) => {
+
+    let newNum = Math.floor(num);
+    let string = String(newNum);
+    let newStr = "";
+
+    if(string.length > 9) {
+        newStr = `${string.substring(0, string.length - 9)}.${string.substring(1,2)}B`;
+    } else if (string.length > 6){
+        newStr = `${string.substring(0, string.length - 6)}.${string.substring(1,2)}M`
+    }else{
+        newStr = string;
+    }
+
+    newStr = "$" + newStr;
+
+    return newStr;
+}
+
+// function to get small decimal number changes as a percentage
+let getChange = (num) =>{
+
+    let numToStr = String(num);
+    let newStr = "";
+
+    for(let i = 0; i < numToStr.length; i++){
+        let curr = numToStr[i];
+        if(curr === "."){
+            newStr = numToStr.substring(0, i+3);
+            newStr += "%";
+            break;
+        }
+    }
+    return newStr;
+}
+
+let greenOrRed = (string, DOMElement) => {
+
+    if(string[0] === "-"){
+        DOMElement.style.color = "red";
+    } else{
+        DOMElement.style.color = "green";
+    }
+}
 
 let getPrices = ()=>{
 
-    let url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cusd-coin%2Cdogecoin%2Carbitrum%2Cgmx%2Cmagic&vs_currencies=usd";
+    let url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cusd-coin%2Cdogecoin%2Carbitrum%2Cgmx%2Cmagic&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true";
 
     console.log(numbers);
 
@@ -25,7 +78,8 @@ let getPrices = ()=>{
             console.log(i);
             number.setAttribute('class','number');
             number.setAttribute('class','dataValue');
-            number.innerText = i + 1;
+            let num = String(i + 1) + "."
+            number.innerText = num;
             numbers.append(number);
 
         }
@@ -43,12 +97,40 @@ let getPrices = ()=>{
             // adding coin prices
             let price = document.createElement('div');
             price.setAttribute('class', 'price');
-            price.setAttribute('class', 'datavalue');
+            price.setAttribute('class', 'dataValue');
             let num = tokenData[key].usd;
             console.log(num);
             num = '$' + String(num);
             price.innerText = num;
-            
+            prices.append(price);
+
+            //adding market cap values
+            let marketCapVal = document.createElement('div');
+            marketCapVal.setAttribute('class', 'marketCap');
+            marketCapVal.setAttribute('class', 'dataValue');
+            let cap = tokenData[key].usd_market_cap;
+            cap = shortNum(cap);
+            marketCapVal.innerText = cap;
+            marketCap.append(marketCapVal);
+
+            // adding trade volume amounts
+            let volume = document.createElement('div');
+            volume.setAttribute('class','volume');
+            volume.setAttribute('class','dataValue');
+            let volumeVal = tokenData[key].usd_24h_vol;
+            volumeVal = shortNum(volumeVal);
+            volume.innerText = volumeVal;
+            volumes.append(volume);
+
+            //add price change percentages
+            let change = document.createElement('div');
+            change.setAttribute('class', 'change');
+            change.setAttribute('class', 'dataValue');
+            let changeVal = tokenData[key].usd_24h_change;
+            changeVal = getChange(changeVal);
+            greenOrRed(changeVal, change);
+            change.innerText = changeVal;
+            changes.append(change);
         }
 
     })
